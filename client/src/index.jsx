b09bdd887c82d4c450eba9c10782720b0900b6b8
Will Mainwaring <willm@yogainternational.com>
2019-07-09 18:32:52 -0400

    complete book and books endpoints

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,27 +6,33 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [],
-      path: 0,
+      book: [],
     };
     this.randomizeBook = this.randomizeBook.bind(this);
   }
-  getBooks() {
-    $.ajax({ method: 'GET', url: 'http://localhost:3030/books' }).then(books => {
-      this.setState({ books });
-      console.log(books);
-    });
+  getBook() {
+    var url = document.URL.slice(27);
+    if (url === '') {
+      $.get('http://localhost:3030/books').then(book => {
+        this.setState({ book });
+      });
+    } else {
+      $.get('http://localhost:3030/' + url).then(book => {
+        console.log('book', book);
+        this.setState({ book });
+      });
+    }
   }
+
   randomizeBook() {
     this.setState({ path: Math.floor(Math.random() * 100) });
   }
   componentDidMount() {
-    this.getBooks();
-    this.randomizeBook();
+    this.getBook();
   }
   render() {
-    const { books, path } = this.state;
-    return <Book randomize={this.randomizeBook} {...books[path]} />;
+    const { book, path } = this.state;
+    return <Book randomize={this.randomizeBook} {...book[0]} />;
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const db = require('../database');
 let app = express();
 const port = 3030;
 
-app.use(express.static(__dirname + '/../public/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/books', (req, res) => {
+app.get('/books', cors(), (req, res) => {
   db.findAll(response => {
-    res.send(response);
+    res.json(response);
+  });
+});
+app.use('/', express.static(__dirname + '/../public/dist'));
+
+
+app.get('/:id', cors(), (req, res) => {
+  const id = req.params.id;
+  console.log('id: ', id);
+  db.find(id, response => {
+    res.json(response);
   });
 });
 
-app.get('/book/:id', (req, res) => {
-  const id = req.params.id;
-  db.find(id, response => {
-    console.log(response);
-    res.send(response);
-  })
-})
+app.use('/book/:id', express.static(__dirname + '/../public/dist'));
 
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
