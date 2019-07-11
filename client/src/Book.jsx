13588c5ec2924ec33979dcb0617fde804f7ef583
Will Mainwaring <willm@yogainternational.com>
2019-07-10 23:25:25 -0400

    set up routes

--- client/src/Book.jsx
+++ client/src/Book.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Book = (book, { randomize }) => {
+const Image = styled.img`
+  max-width: 150px'
+  height: auto;
+`;
+
+const Book = book => {
   return (
-    <div className="ml-2 mt-5">
+    <div className="container ml-2 mt-5">
       <div className="row">
-        <div className="col-2">
-          <img src={book.image} />
+        <div className="col-4">
+          <Image src={book.image} />
         </div>
-        <div className="col-8">
+        <div className="col-6 text-left">
           <h3>{book.title}</h3>
           {book.author && <p> by {book.author.name}</p>}
           <p>Rating: {book.rating}</p>
           <p>{book.description}</p>
         </div>
       </div>
-      <div>
-        <button
-          type="submit"
-          onClick={() => {
-            randomize();
-          }}
-        >
-          Random Book
-        </button>
-      </div>
+      <div />
     </div>
   );
 };
